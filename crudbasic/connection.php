8082c4f2ec6977581d0693c247d93e6858988628
Mandeep <mandeep_singh9398@yahoo.in>
2021-02-13 00:58:32 +0530

    Update connection file

--- crudbasic/connection.php
+++ crudbasic/connection.php
@@ -9,7 +9,7 @@ $con = mysqli_connect($server, $username, $password, $db);
 
 if ($con) {
   ?>
-  <script>setTimeout(() => {alert("DB Connection Successful!")}, 1200);</script>
+  <script>setTimeout(() => {alert("DB Connection Successful!")}, 1000);</script>
   <?php
 } else {
   echo 'connection failed!';
